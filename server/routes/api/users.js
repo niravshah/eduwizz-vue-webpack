af268501750769bd9db97030ff6b39b3e016ba25
shahnirav <shah.nirav@me.com>
2017-09-25 21:06:48 +0100

    commit

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -15,6 +15,20 @@ module.exports = function (passport) {
     })
   })
 
+  router.get('/api/admin/user/:sid', (req, res) => {
+    User.findOne({sid: req.params.sid}, function (err, user) {
+      if (err) {
+        res.status(500).json({message: 'Error fetching user'})
+      } else {
+        if (user) {
+          res.json({user: user})
+        } else {
+          res.status(500).json({message: 'No user found with that id'})
+        }
+      }
+    })
+  })
+
   router.post('/api/admin/users', (req, res) => {
     User.findOne({username: req.body.username}, function (err, user) {
       if (err) {
--- src/components/User-Delete.vue
+++ src/components/User-Delete.vue
@@ -1,12 +1,68 @@
 <template>
   <div>
     <h3><i class="fa fa-angle-right"></i> Delete User</h3>
+    <div class="row mt">
+      <div class="col-lg-12">
+
+        <div class="row pb10">
+          <label class="col-sm-2 col-sm-2 control-label">Name</label>
+          <div class="col-sm-6">
+            <p>{{user.name}}</p>
+          </div>
+        </div>
+
+        <div class="row pb10">
+          <label class="col-sm-2 col-sm-2 control-label">Username</label>
+          <div class="col-sm-6">
+            <p>{{user.username}}</p>
+          </div>
+        </div>
+        <div class="row pb10">
+          <label class="col-sm-2 col-sm-2 control-label">User Email</label>
+          <div class="col-sm-6">
+            <p>{{user.email}}</p>
+          </div>
+        </div>
+        <div class="row pb10">
+          <label class="col-sm-2 col-sm-2 control-label">Permissions</label>
+          <div class="col-sm-6">
+            <label class="checkbox-inline">
+              <input type="checkbox" id="maths" disabled="disabled" name="maths" v-model="user.permissions.maths"> Maths
+            </label>
+            <label class="checkbox-inline">
+              <input type="checkbox" id="physics" disabled="disabled" name="physics" v-model="user.permissions.physics"> Physics
+            </label>
+            <label class="checkbox-inline">
+              <input type="checkbox" id="chemistry" disabled="disabled" name="chemistry" v-model="user.permissions.chemistry"> Chemistry
+            </label>
+            <label class="checkbox-inline">
+              <input type="checkbox" id="biology" disabled="disabled" name="biology" v-model="user.permissions.biology"> Biology
+            </label>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 
 </template>
 <script>
+  import axios from 'axios'
   export default {
-    name: 'User-Delete'
+    name: 'User-Delete',
+    data: function () {
+      return {
+        user: ''
+      }
+    },
+    created: function () {
+      var url = '/api/admin/user/' + this.$route.query.sid
+      axios.get(url).then(res => {
+        this.user = res.data.user
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   }
 </script>
 <style></style>
