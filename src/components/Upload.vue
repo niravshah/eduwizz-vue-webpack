d5af30a1e4602d12d9661c0a7b457ef1dc1f110b
shahnirav <shah.nirav@me.com>
2017-10-27 22:20:11 +0100

    commit

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -46,26 +46,38 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <button v-if="!uploading" v-bind:disabled="uploading" type="button" class="close" data-dismiss="modal">
+              &times;
+            </button>
             <h4 class="modal-title">Are you sure?</h4>
           </div>
           <div class="modal-body">
-            <p>Some text in the modal.</p>
-            <radial-progress-bar :diameter="200"
-                                 :completed-steps="completedSteps"
-                                 :total-steps="totalSteps"
-                                 :startColor="accentColor"
-                                 :stopColor="accentColor"
-                                 style="margin: auto">
-              <p>Total: {{ totalSteps }}</p>
-              <p>Completed: {{ completedSteps }}</p>
-            </radial-progress-bar>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <div v-on:click="modalSend()" class="btn btn-theme" style="width: 100px"><i class="fa fa-send"></i> Yes
+            <div v-if="!uploading">
+              <p>Total Files to send: {{files.length}}</p>
             </div>
+            <transition name="fade">
+              <div v-if="uploading">
+                <radial-progress-bar :diameter="200"
+                                     :completed-steps="completedSteps"
+                                     :total-steps="totalSteps"
+                                     :startColor="accentColor"
+                                     :stopColor="accentColor"
+                                     style="margin: auto">
+                  <p>Total: {{ totalSteps }}</p>
+                  <p>Completed: {{ completedSteps }}</p>
+                </radial-progress-bar>
+              </div>
+            </transition>
           </div>
+          <transition name="fade">
+            <div v-if="!uploading" class="modal-footer">
+              <button v-bind:disabled="uploading" type="button" class="btn btn-default" data-dismiss="modal">Close
+              </button>
+              <div v-bind:disabled="uploading" v-on:click="modalSend()" class="btn btn-theme" style="width: 100px"><i
+                class="fa fa-send"></i> Yes
+              </div>
+            </div>
+          </transition>
         </div>
       </div>
     </div>
@@ -86,7 +98,8 @@
         uploadErrors: [],
         completedSteps: 2,
         totalSteps: 5,
-        accentColor: '#FF4081'
+        accentColor: '#FF4081',
+        uploading: false
       }
     },
     computed: {
@@ -105,11 +118,6 @@
           this.okToSend = false
           var _this = this
           axios.get(url).then(res => {
-            /* let data = new FormData()
-             data.append('file', file)
-             axios.put(res.data.signedData, data).then(resp => {}).catch(err => {
-             console.log(err)
-             }) */
             var reader = new FileReader()
             reader.readAsDataURL(file)
             reader.onload = function (e) {
@@ -162,78 +170,46 @@
           var context = canvas.getContext('2d')
           context.drawImage(this, 0, 0, newWidth, newHeight)
 
-          var dataUrl = canvas.toDataURL('image/jpeg')
-
-          /* if (canvas.toBlob) {
-           canvas.toBlob(
-           function (blob) {
-           var dat = {}
-           dat['file'] = blob
-           dat['url'] = signedUrl
-           _this.files.push(dat)
-           },
-           fileType
-           )
-           } */
-          var resizedImage = _this.dataURLToBlob(dataUrl)
-          var dat2 = {}
-          dat2['name'] = name
-          dat2['file'] = resizedImage
-          dat2['url'] = signedUrl
-          _this.files.push(dat2)
+          if (canvas.toBlob) {
+            canvas.toBlob(
+              function (blob) {
+                var dat = {}
+                dat['file'] = blob
+                dat['url'] = signedUrl
+                _this.files.push(dat)
+              },
+              fileType
+            )
+          }
         }
         image.onerror = function () {
           alert('There was an error processing your file!')
         }
       },
       sendAnswerSheets: function () {
-        var _this = this
-        _this.$modal.show('hello-world')
-        console.log('About to send these files to S3:')
-
         for (var i = 0; i < this.files.length; i++) {
           var dat = this.files[i]
-          // let data = new FormData()
-          // data.append('file', dat['file'])
           let signedUrl = dat['url']
           let data = dat['file']
-          axios.put(signedUrl, data).then(resp => {
+          var config = {
+            onUploadProgress: function (progressEvent) {
+              var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+              console.log('Upload Progress: ', percentCompleted)
+            }
+          }
+          axios.put(signedUrl, data, config).then(resp => {
             console.log('received response')
             console.log(resp)
           }).catch(err => {
-            // alert('There was an error processing your file!')
-            _this.uploadErrors.push(err.message)
             console.log('received error')
             console.log(err)
           })
         }
       },
-      dataURLToBlob: function (dataURL) {
-        var BASE64_MARKER = ';base64,'
-        var parts, contentType, raw
-        if (dataURL.indexOf(BASE64_MARKER) === -1) {
-          parts = dataURL.split(',')
-          contentType = parts[0].split(':')[1]
-          raw = parts[1]
-
-          return new Blob([raw], {type: contentType})
-        }
-
-        parts = dataURL.split(BASE64_MARKER)
-        contentType = parts[0].split(':')[1]
-        raw = window.atob(parts[1])
-        var rawLength = raw.length
-
-        var uInt8Array = new Uint8Array(rawLength)
-
-        for (var i = 0; i < rawLength; ++i) {
-          uInt8Array[i] = raw.charCodeAt(i)
-        }
-
-        return new Blob([uInt8Array], {type: contentType})
-      },
       modalSend: function () {
+        this.uploading = true
         console.log('Sending Files')
+        this.sendAnswerSheets()
       }
     },
     components: {
@@ -282,5 +258,14 @@
 
   .file-preview-input-title {
     margin-left: 2px;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 2s
+  }
+
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+  {
+    opacity: 0
   }
 </style>
